046c90bbf14267a71df75375d7815132f2b5464f
AnubrataDS <anubrata.stephenite.nrg@gmail.com>
2021-04-20 14:09:03 +0530

    random scene generation

--- js/generate.js
+++ js/generate.js
@@ -58,29 +58,69 @@ function write_color(col, samples_per_pixel) {
   );
   return toneMap(newCol);
 }
+
 //Generate the image
 //generates a smooth gradient for now
 function generate() {
   //world(scene)
   var world = new hittable_list();
-  var material_ground = new lambertian(new color(0.8, 0.8, 0.0));
+
+  //ground
+  var material_ground = new lambertian(
+    new color(
+      0.5 + random_ranged(-0.2, 0.2),
+      0.5 + random_ranged(-0.2, 0.2),
+      0.5 + random_ranged(-0.2, 0.2)
+    )
+  );
+  world.add(new sphere(new point3(0.0, -1000, 0), 1000.0, material_ground));
+
+  //small random spheres
+  for (var i = -2; i < 2; i++) {
+    var choose_mat = Math.random();
+    var radius = random_ranged(0.1, 0.5);
+    var center = new vec3(random_ranged(-4, 4), radius, random_ranged(0, 2));
+    if (subtract(center, new point3(4, 0.2, 0)).length() > 0.9) {
+      if (choose_mat < 0.8) {
+        // diffuse
+        var albedo = new color(Math.random(), Math.random(), Math.random());
+        var sphere_material = new lambertian(albedo);
+        world.add(new sphere(center, radius, sphere_material));
+      } else if (choose_mat < 0.95) {
+        // metal
+        var albedo = new color(
+          random_ranged(0.5, 1),
+          random_ranged(0.5, 1),
+          random_ranged(0.5, 1)
+        );
+        var fuzz = random_ranged(0, 0.5);
+        var sphere_material = new metal(albedo, fuzz);
+        world.add(new sphere(center, radius, sphere_material));
+      } else {
+        // glass
+        var sphere_material = new dielectric(1.5);
+        world.add(new sphere(center, radius, sphere_material));
+      }
+    }
+  }
+  console.log("spheres done");
+  //big spheres
   var material_center = new lambertian(new color(0.1, 0.2, 0.5));
   var material_left = new dielectric(1.5);
   var material_right = new metal(new color(0.8, 0.6, 0.2), 0.1);
 
-  world.add(new sphere(new point3(0.0, -100.5, -1.0), 100.0, material_ground));
-  world.add(new sphere(new point3(0.0, 0.0, -1.0), 0.5, material_center));
-  world.add(new sphere(new point3(-1.0, 0.0, -1.0), 0.5, material_left));
+  world.add(new sphere(new point3(0, 1, 0), 1.0, material_center));
+  world.add(new sphere(new point3(-4, 1, 0), 1.0, material_left));
   //world.add(new sphere(new point3(-1.0, 0.0, -1.0), -0.4, material_left));
-  world.add(new sphere(new point3(1.0, 0.0, -1.0), 0.5, material_right));
+  world.add(new sphere(new point3(4, 1, 0), 1.0, material_right));
   //camera settings
-  var samples_per_pixel = 15; //More makes image better but generation is much slower
-  var max_depth = 100; //recursion depth for ray bouncing, more means less black spots
-  var lookfrom = new point3(-2, 2, 2);
-  var lookat = new point3(0, 0, -1);
+  var samples_per_pixel = 10; //More makes image better but generation is much slower
+  var max_depth = 10; //recursion depth for ray bouncing, more means less black spots
+  var lookfrom = new point3(-13, 2 + random_ranged(0, 5), 3);
+  var lookat = new point3(0, 0, 0);
   var vup = new point3(0, 1, 0);
-  var dist_to_focus = subtract(lookfrom, lookat).length();
-  var aperture = 0.2;
+  var dist_to_focus = 10.0;
+  var aperture = 0.1;
   var aspect_ratio = canvasWidth / canvasHeight;
   var cam = new camera(
     lookfrom,
